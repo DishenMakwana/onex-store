fe87153598f540dabb87addae4a394703959eb66
Dishen Makwana <dishenmakwana.dm@gmail.com>
2022-07-29 15:09:06 +0530

    ADD: product related apis

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,10 @@ const {
   testProduct,
   addProduct,
   getAllProduct,
+  adminGetAllProduct,
+  getOneProduct,
+  adminUpdateOneProduct,
+  adminDeleteOneProduct,
 } = require('../controllers/productController');
 const { isLoggedIn, customRole } = require('../middlewares/user');
 
@@ -11,10 +15,20 @@ const router = express.Router();
 // user
 router.route('/testProduct').get(testProduct);
 router.route('/products').get(getAllProduct);
+router.route('/product/:id').get(getOneProduct);
 
 // admin
 router
   .route('/admin/product/add')
   .post(isLoggedIn, customRole('admin'), addProduct);
+router
+  .route('/admin/products')
+  .get(isLoggedIn, customRole('admin'), adminGetAllProduct);
+router
+  .route('/admin/product/:id')
+  .put(isLoggedIn, customRole('admin'), adminUpdateOneProduct);
+router
+  .route('/admin/product/:id')
+  .delete(isLoggedIn, customRole('admin'), adminDeleteOneProduct);
 
 module.exports = router;
